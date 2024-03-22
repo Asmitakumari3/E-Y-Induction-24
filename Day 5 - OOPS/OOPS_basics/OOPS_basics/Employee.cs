fd042455b3aaa7ec4f021b0cfa5ad7ec38646918
Nikhil Shah <nikhilshah.net@hotmail.com>
2024-03-22 12:53:23 +0530

    Day 5 - OOPS

---
+++ Day 5 - OOPS/OOPS_basics/OOPS_basics/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_basics
+{
+    internal class Employee
+    {
+        //Variables
+        //by default variables in a class are private and they should be kept private
+        int v_empNo;
+        string v_empName;
+        double v_empSalary;
+        bool v_empIsPermenant;
+        public int v_empAge = 20;
+        //constructor
+        public Employee() 
+        {
+            v_empNo = 101;
+            v_empName = "Nikhil";
+            v_empSalary = 1000;
+            v_empIsPermenant = true;
+            //this values we generally do not hardcode, we pull them from a datatabase or collect them from user
+        }
+
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine("Employee Number " + v_empNo);
+            Console.WriteLine("Employee Name " + v_empName);
+            Console.WriteLine("Employee Salary " + v_empSalary);
+            Console.WriteLine("Employee Is Permenant " + v_empIsPermenant);
+        }
+        public double IcrementSalary()
+        {
+           // Console.WriteLine("Old Salary " + v_empSalary);
+            v_empSalary = v_empSalary + 3000;
+          //  Console.WriteLine("New Salary " + v_empSalary);
+            return v_empSalary; 
+        }
+
+    }
+
+
+    
+}
+
+
+
+
---
+++ Day 5 - OOPS/OOPS_basics/OOPS_basics/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using Investment;
+using OOPS_basics;
+using OOPS_basics.Accounts;
+using OOPS_basics.Investment;
+#region rough code
+//BankAccount acc = new BankAccount();
+
+//Employee emp1 = new Employee();
+
+
+
+//BankAccount bankAccount2 = new BankAccount();
+//BankAccount bankAccount3 = new BankAccount();
+//BankAccount bankAccount4 = new BankAccount();
+
+
+
+//Console.WriteLine("Welcome to OOPS");
+#endregion
+
+
+
+
+//create a new object of Employee
+
+
+
+Employee empObj = new Employee();
+empObj.DisplayEmployeeInfo();
+double result = empObj.IcrementSalary();
+
+Console.WriteLine("New Salary is " + result);
+
+Console.WriteLine("Enter First Number");
+int n1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int n2 =Convert.ToInt32(Console.ReadLine());
+
+int addResult = empObj.Add(n1, n2);
+Console.WriteLine("Addition is " + addResult);
+
+GOLD
