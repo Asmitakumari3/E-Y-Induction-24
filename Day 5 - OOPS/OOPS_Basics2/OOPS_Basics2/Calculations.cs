dfc04f1bdca19a400143a05ec5c8d1a859362a17
Nikhil Shah <nikhilshah.net@hotmail.com>
2024-03-22 17:33:08 +0530

    Day 5 - Demos

---
+++ Day 5 - OOPS/OOPS_Basics2/OOPS_Basics2/Calculations.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Basics2
+{
+  
+
+
+    internal class Calculations
+    {
+        //Method Overloading
+        //Where name of the method is same, but parameters, return type, number of parameters are different
+        //method overload can exist in same class or even when you have inheritance
+        public int Add(int num1, int num2)
+        { 
+            return num1 + num2;
+        }
+
+        public string Add(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+        public double Add(int num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Add(int num1, int num2, int num3) 
+        {
+            return num1 + num2 + num3;
+        }
+
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        //there is a feature for C# - call Params Array
+
+
+        public int Add(int num1, int num2,params int[] moreNumbers)
+        {
+            int result = num1 + num2;
+            for (int i = 0; i < moreNumbers.Length; i++)
+            {
+                result = result + moreNumbers[i];
+            }
+            return result;
+        }
+
+        //default parameter value
+        //named parameter
+        
+        public string AddEmployee(string empName,string lastName,string designation = "New Joinee", string dept = "Training")
+        {
+            //we will do some program like add to database
+            return "Ëmployee Added Successfully" + empName + " is a " + designation + " in " + dept + " department";
+        }
+
+
+        public int CalculateNumbers(int num1, int num2,OperationType whichOperation) 
+        {
+            if(whichOperation.Equals(OperationType.Add)) 
+            {
+                return num1 + num2;
+            }
+            else if(whichOperation.Equals(OperationType.Subtract))
+            {
+                return num1 - num2;
+            }
+            else if(whichOperation.Equals(OperationType.Multiply))
+            {
+                return num1 * num2;
+            }
+            else if(whichOperation.Equals(OperationType.Divide))
+            {
+                return num1 / num2;
+            }
+            return 0;
+        }    
+    }
+    enum OperationType
+    {
+        Add, Subtract, Divide, Multiply
+    }
+}
---
+++ Day 5 - OOPS/OOPS_Basics2/OOPS_Basics2/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using OOPS_Basics2;
+
+Console.WriteLine("Hello, World!");
+
+Calculations calObj = new Calculations();
+//int addResult = calObj.Add(10, 20, 30);
+
+//Console.WriteLine(calObj.Add("Nikhil", "Shah"));
+
+//Console.WriteLine(addResult);
+
+//int result = calObj.Add(10, 20, 30, 40, 50, 60, 70, 70, 50, 30);
+//Console.WriteLine(result);
+
+//Console.WriteLine(calObj.AddEmployee("Jay","Shah"));
+//Console.WriteLine(calObj.AddEmployee("Ankita","Jaiswal",dept:"HR"));
+//Console.WriteLine(calObj.AddEmployee("Hussain","Ahmed",dept:"Development",designation:"Front End programmer"));
+//Console.WriteLine(calObj.AddEmployee("Prachi","Shah","Development","UI Designer"));
+
+Console.WriteLine(calObj.CalculateNumbers(20, 10, OperationType.Add));
+
+
+
+
+//Employee employeeObj = new Employee();
+
+//employeeObj.EmpNo = 105; // this is the set part
+//Console.WriteLine(employeeObj.EmpNo); //this is get part which will return value stored in v_empNo
+
+
+
+
+
+
+
+
+Console.WriteLine("How many employees you want to generate ? ");
+int totalEmpObjs = Convert.ToInt32(Console.ReadLine());
+
+List<Employee> empList = new List<Employee>();
+Employee empObj = new Employee();
+
+for (int i = 0; i < totalEmpObjs; i++)
+{
+    empObj = new Employee();
+    empObj.EmpNo = i;
+ 
+    Console.WriteLine("Enter name of employee no " + (i + 1));
+    empObj.EmpName = Console.ReadLine();
+    Console.WriteLine("Enter Salary");
+
+    empObj.EmployeeSalary = Convert.ToDouble(Console.ReadLine());
+
+    empList.Add(empObj);   
+}
+
+double totalSalary = 0;
+foreach (var item in empList)
+{
+    totalSalary = totalSalary + item.EmployeeSalary;
+    Console.WriteLine(item.EmpNo);
+    Console.WriteLine(item.EmpName);
+    Console.WriteLine("------------------------------");
+
+}
+
+Console.WriteLine("Total Salary Paid is " + totalSalary);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
