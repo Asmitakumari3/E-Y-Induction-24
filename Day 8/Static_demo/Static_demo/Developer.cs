160dd2b9a5c8694b632e915851afcc0d3db96fdb
Nikhil Shah <nikhilshah.net@hotmail.com>
2024-03-27 17:09:28 +0530

    Day 8

---
+++ Day 8/Static_demo/Static_demo/Developer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_demo
+{
+    internal class Developer : EmployeeInfo
+    {
+
+        static int sv_autoDeveloperNo = 900;
+
+        public Developer(string p_empname, string p_empdesigantion) : base(p_empname, p_empdesigantion)
+        {
+            sv_autoDeveloperNo = sv_autoDeveloperNo + 1;
+            empNo = (p_empdesigantion.Substring(0, 2) + sv_autoDeveloperNo).ToUpper();
+        }
+    }
+}
---
+++ Day 8/Static_demo/Static_demo/HR.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_demo
+{
+    internal class HR : EmployeeInfo
+    {
+        static int sv_autoNumberHR = 100;
+        public HR(string p_empname,string p_empdesigantion):base(p_empname, p_empdesigantion)
+        {
+            sv_autoNumberHR = sv_autoNumberHR + 1;
+            empNo = (p_empdesigantion.Substring(0,2) +  sv_autoNumberHR).ToUpper();
+        }
+    }
+}
