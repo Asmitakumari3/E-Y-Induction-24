ef2a0100f20e15c4c1dc9625db121ebe8e0bcf45
unknown <nikhilshah.net@hotmail.com>
2024-04-19 17:08:33 +0530

    Day 22

---
+++ Day 22/LINQ_SP_Demo/LINQ_SP_Demo/Program.cs
@@ -0,0 +1,61 @@
+﻿using LINQ_SP_Demo.db;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+
+#region SP 1 - select *
+//InsuranceDbSpLinqContext db = new InsuranceDbSpLinqContext();
+
+////var data = db.Database.ExecuteSqlRaw("proc_allPolicies");
+//var data = db.PolicyDetails.FromSql($"execute proc_allPolicies");
+
+//foreach (var item in data)
+//{
+//    Console.WriteLine(item.PolicyNo);
+//    Console.WriteLine(item.PolicyName);
+//    Console.WriteLine(item.PolicyPremieum);
+//}
+
+#endregion
+
+#region SP 2 - select by policy No
+
+//InsuranceDbSpLinqContext db  = new InsuranceDbSpLinqContext();
+
+//Console.WriteLine("Enter Policy No");
+//int policyNo = Convert.ToInt32(Console.ReadLine());
+//var data = db.PolicyDetails.FromSql($"execute proc_policyByNo {policyNo}");
+
+
+//int count = 0;
+//foreach ( var item in data )
+//{
+//    count++;
+//    Console.WriteLine(item.PolicyNo);
+//    Console.WriteLine(item.PolicyName);
+//    Console.WriteLine(item.PolicyPremieum);
+//}
+//if (count ==0)
+//{
+//    Console.WriteLine("Policy Not found");
+
+//}
+
+
+#endregion
+
+
+#region SP - insert new record
+InsuranceDbSpLinqContext db = new InsuranceDbSpLinqContext();
+var data = db.PolicyDetails.FromSqlInterpolated($"execute proc_NewPolicy {140},{"Medical Ins"},{18000}");
+
+db.SaveChanges();
+Console.WriteLine("Policy Added");
+
+
+
+#endregion
+
+
+
+
---
+++ Day 22/LINQ_SP_Demo/LINQ_SP_Demo/db/InsuranceDbSpLinqContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LINQ_SP_Demo.db;
+
+public partial class InsuranceDbSpLinqContext : DbContext
+{
+    public InsuranceDbSpLinqContext()
+    {
+    }
+
+    public InsuranceDbSpLinqContext(DbContextOptions<InsuranceDbSpLinqContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<PolicyDetail> PolicyDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=86B46DA2A2A958B;database=insuranceDB_SP_LINQ;integrated security=true;trustservercertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PolicyDetail>(entity =>
+        {
+            entity.HasKey(e => e.PolicyNo).HasName("PK__policyDe__78E1E82237872AC9");
+
+            entity.ToTable("policyDetails");
+
+            entity.Property(e => e.PolicyNo)
+                .ValueGeneratedNever()
+                .HasColumnName("policyNo");
+            entity.Property(e => e.PolicyName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("policyName");
+            entity.Property(e => e.PolicyPremieum).HasColumnName("policyPremieum");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
