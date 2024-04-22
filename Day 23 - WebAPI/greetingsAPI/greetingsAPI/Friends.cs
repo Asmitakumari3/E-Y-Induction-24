d72c38c7cc151b328faae7ad14ea8057d3d0f15b
Nikhil Shah <nikhilshah.net@hotmail.com>
2024-04-22 17:21:10 +0530

    Day 23 - Rest API

---
+++ Day 23 - WebAPI/greetingsAPI/greetingsAPI/Friends.cs
@@ -0,0 +1,13 @@
+﻿namespace greetingsAPI
+{
+    public class Friends
+    {
+        public string friendName { get; set; }
+        public double friendMobile { get; set; }
+        public string friendEmail { get; set; }
+        public string friendAddress { get; set; }
+
+        public string friendNickName { get; set; }
+
+    }
+}
